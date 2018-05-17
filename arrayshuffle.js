1011710ae9c0ccbb385d1e87033ed1a0a634ac63
Michael James Albatana <dev.mjta@gmail.com>
2018-05-18 03:52:36 +0800

    DEV v1.0.0.1 removed bloatful codes

--- arrayshuffle.js
+++ arrayshuffle.js
@@ -9,10 +9,7 @@ function arrayShuffle(array){
   }
   for(l=0; l<length; l++){
     var n = parseInt(Math.random()*(arrayi_ref.length));
-    console.log(n);
     arrayi_shuffle.push(arrayi_ref.splice(n,1));
-    console.log('ref:'+arrayi_ref);
-    console.log('gen:'+arrayi_shuffle);
   }
   for(l=0; l<length; l++){
     array_new.push(array[arrayi_shuffle[l][0]]);
