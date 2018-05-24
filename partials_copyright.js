92bf4266c5ecb66ccc73c72615f7ccee3e31208d
Michael James Albatana <dev.mjta@gmail.com>
2018-05-25 03:46:24 +0800

    DEV v1.1.0.0 minified "partials_copyright" by removing class name

--- partials_copyright.js
+++ partials_copyright.js
@@ -1,9 +1,8 @@
 jsCopyright('Michael James Turiano Albatana');
 function jsCopyright(author){
   var date = new Date(),
-      text = 'Copyright + &copy;' + date.getFullYear() + '&nbsp;' + author,
+      text = 'Copyright' + '&nbsp;' + '&copy;' + date.getFullYear() + '&nbsp;' + author,
       footer = document.createElement('footer');
-  footer.id = 'footer-copyright';
   footer.innerHTML = text;
   document.getElementsByTagName('body')[0].insertBefore(footer,null);
 }
