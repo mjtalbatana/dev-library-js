b93a40d505a032c23c5c0f0ec501c67b9399f203
Michael James Albatana <dev.mjta@gmail.com>
2018-06-16 01:31:56 +0800

    DEV v1.2.0.0 OPTIMIZE [arrayshuffle.js] @ var streamlining

--- assetloader_template.js
+++ assetloader_template.js
@@ -5,16 +5,16 @@
   var parameters =
   [
     {
-      "url_local": "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js",
-      "url_cdn": "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js",
-      "defer": null,
-      "async": null
+      url_local: "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js",
+      url_cdn: "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js",
+      defer: null,
+      async: null,
     },
     {
-      "url_local": "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js",
-      "url_cdn": "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js",
-      "defer": null,
-      "async": null
+      url_local: "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js",
+      url_cdn: "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js",
+      defer: null,
+      async: null,
     }
   ];
 
@@ -23,48 +23,57 @@
 
   // PLEASE DO NOT MODIFY ANYTHING PRECEEDING THIS LINE OF CODE =================
   //DECLARATIONS
-  var script1 = document.createElement('script'),
-      script2 = document.createElement('script'),
-      script3 = document.createElement('script'),
-      script4 = document.createElement('script'),
-      script5 = document.createElement('script'),
-      script6 = document.createElement('script'),
-      scripts = [],
+  var script = [],
       address = window.location.origin;
 
+      script[1] =
+      script[2] =
+      script[3] =
+      script[4] =
+      script[5] =
+      script[6] = document.createElement('script');
+
+
   //LOCAL/WEB CHECK
-  if(address.search('127.0.0.1') == -1 || address.search('localhost') == -1){
+  if(address.search('127.0.0.1') === -1 || address.search('localhost') === -1){
     address = 'localserver';
   } else{
     address = 'webserver';
   }
 
   //TYPE ASSIGNMENT
-  script1.type = script2.type = script3.type = script4.type = script5.type = script6.type = 'application/javascript';
+  script[1].type =
+  script[2].type =
+  script[3].type =
+  script[4].type =
+  script[5].type =
+  script[6].type = 'application/javascript';
 
   //DEFER ASSIGNMENT
-  script3.defer = script4.defer = script5.defer = script6.defer = true;
+  script[3].defer =
+  script[4].defer =
+  script[5].defer =
+  script[6].defer = true;
 
   //ASYNC ASSIGNMENT
   // node.async = true;
 
 
   if(address == 'localserver'){
-    script1.src = "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js";
-    script2.src = "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js";
+    script[1].src = "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js";
+    script[2].src = "../../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js";
   } else{
-    script1.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js";
-    script2.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js";
+    script[1].src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js";
+    script[2].src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js";
   }
 
-  script3.src = "assets/js/script.js";
-  script4.src = "assets/layouts.js";
-  script5.src = "assets/js/animation.js";
-  script6.src = "assets/js/interactive.js";
+  script[3].src = "assets/js/script.js";
+  script[4].src = "assets/layouts.js";
+  script[5].src = "assets/js/animation.js";
+  script[6].src = "assets/js/interactive.js";
 
-  scripts = [script1, script2, script3, script4, script5, script6];
-  for(l=0; l<scripts.length; l++){
-    document.getElementsByTagName('head')[0].insertBefore(scripts[l],null);
+  for(l=0; l<script.length; l++){
+    document.getElementsByTagName('head')[0].insertBefore(script[l],null);
   }
 })();
 
