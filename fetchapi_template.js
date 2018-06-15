6b5fdd90cd4987ef688ca5fef3b5b7476b6b2d4a
Michael James Albatana <dev.mjta@gmail.com>
2018-06-15 12:16:45 +0800

    DEV v1.0.0.0 PUBLISH fetchapi_template.js

---
+++ fetchapi_template.js
@@ -0,0 +1,72 @@
+function fetchapi(array, action, targetid='snackbar'){
+  /*
+    $array =
+    [
+      url
+      init
+
+    ]
+  */
+
+  array = {
+    formkey: [
+      'key'
+    ],
+    formval: [
+      'value'
+    ],
+    url: "assets/php/fetchapi.php",
+    method: "POST",
+    credentials: "same-origin",
+    /*
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+    */
+  }
+
+  var formData = new FormData();
+
+  for(l=0; l<array['formkey'].length; l++){
+    formData.append(array['formkey'][l], array['formvalue'][l]);
+  }
+
+  fetch(url, {
+    method: array['method'],
+    body: formData,
+    credentials: array['credentials']
+  })
+  .then(response => response.text())
+  .then(function(response){
+    var data = JSON.parse(response);
+    /*
+      data should be in form of associative array:
+      array->sub0->datalist
+      array->sub1->datalist
+    */
+
+
+    for(l=0; l<data.length; l++){
+      for(k=0; k<data['sub'+l]; k++){
+        //actions to datalists
+      }
+    }
+
+
+    // ASYNC actions
+    //
+
+  })
+  .catch(
+    console.log('asynchronous fetch-API function failed!')
+  );
+
+
+}
