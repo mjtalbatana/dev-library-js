7b427ba5b166d07d73458c34130a6bffeb90b609
Michael James Albatana <dev.mjta@gmail.com>
2018-07-09 01:50:36 +0800

    DEV [assetloader_template.js] v1.2.0.0 REVISE code, see...
    - enabled 100% library-integration
    - separtion of code and data

--- assetloader_template.js
+++ assetloader_template.js
@@ -1,102 +1,65 @@
-//SELF-INVOKED FUNCTION
+//SELF-INVOKED FUNCTION [assetloader.js]
+/*
+compatibility:
+ Laravel
+ Static JavaScript
 
-(function(){
-  // PARAMETER INPUTS: =========================================================
-    /*----------------------------
-    FOR JAVASCRIPT
+notes:
+ data-array is placed statically at the <head><script> portion of the DOM
+ markup may be partialized as long as the data & callout-function is enclosed together in a <script> element
+
+*/
+// PARAMETER INPUTS: ====================================================================
+/*----------------------------
+[
+  FOR NON-JAVASCRIPT
+  [
     {
-      url_primary: null,
-      url_cdn: null,
-      defer: null,
-      async: null
-    },
-    FOR NON-JAVASCRIPT
-    {
-      href_primary: null,
-      href_cloud: null,
+      href:
+      [
+        null, // for multiple sources, index=0 is the localized path of the resource
+        null,
+      ],
+      integrity: null,
+      cross-origin: null,
       rel: null,  // author, help, icon, license, stylesheet
       type: null, // MIME-classification
     },
-    ----------------------------*/
-  var parameter_a =
-  [
     {
-      url_primary: "../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js",
-      url_cdn: "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js",
-      defer: null,
-      async: null
-    },
-    {
-      url_primary: "../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js",
-      url_cdn: "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js",
-      defer: null,
-      async: null
-    },
-    {
-      url_primary: "assets/js/script.js",
-      url_cdn: null,
-      defer: true,
-      async: null
-    },
-    {
-      url_primary: "assets/layouts.js",
-      url_cdn: null,
-      defer: true,
-      async: null
-    },
-    {
-      url_primary: "assets/js/animation.js",
-      url_cdn: null,
-      defer: true,
-      async: null
-    },
-    {
-      url_primary: "assets/js/interactive.js",
-      url_cdn: null,
-      defer: true,
-      async: null
-    },
-    {
-      url_primary: "../dev-library-js/imageloader.js",
-      url_cdn: "https://raw.githubusercontent.com/mjtalbatana/dev-library-js/master/imageloader.js",
-      defer: null,
-      async: null
-    },
-    {
-      url_primary: "../dev-library-js/arrayshuffle.js",
-      url_cdn: "https://raw.githubusercontent.com/mjtalbatana/dev-library-js/master/arrayshuffle.js",
-      defer: null,
-      async: null
-    },
-    {
-      url_primary: "../dev-library-js/linkloader.js",
-      url_cdn: "https://raw.githubusercontent.com/mjtalbatana/dev-library-js/master/linkoader.js",
-      defer: null,
-      async: null
-    },
-  ],
-    parameter_b =
-  [
-    {
-      href_primary: "../dev-assets-common/mjta-v01.ico",
-      href_cloud: "https://raw.githubusercontent.com/mjtalbatana/profile-logo/master/mjta-v01.ico",
-      rel: "icon",
-      type: "image/x-icon" // MIME-classification
-    },
-    {
-      href_primary: "../dev-assets/font/NotoSans/NotoSans-Regular.ttf",
-      href_cloud: "https://fonts.googleapis.com/css?family=Noto+Sans",
-      rel: 'stylesheet',  // author, help, icon, license, stylesheet
-      type: 'font/ttf', // MIME-classification
+      ...
     },
   ]
 
+  FOR JAVASCRIPT
+  [
+    {
+      src:
+      [
+        null, // for multiple sources, index=0 is the localized path of the resource
+        null,
+      ],
+      integrity: null,
+      cross-origin: null,
+      defer: null,
+      async: null
+    },
+    {
+      ...
+    }
+  ]
+]
+----------------------------*/
 
-  // PLEASE DO NOT MODIFY ANYTHING PRECEEDING THIS LINE OF CODE =================
+// PLEASE DO NOT MODIFY ANYTHING PRECEEDING THIS LINE OF CODE ===========================
+
+function assetloader(data){
 
   //DECLARATIONS-------------------------------------------------------------------------
-  var temp = '';
-      scripts = [],
+  var temp = '',
+      attrib = '',
+      value = '',
+      markup = [],
+      mode = '',
       address = window.location.origin;
 
   //LOCAL/WEB CHECK----------------------------------------------------------------------
@@ -107,43 +70,43 @@
   }
 
 
-  //MARKUP AND ATTRIBUTE BUILDER---------------------------------------------------------
-  for(l=0; l<parameter_a.length; l++){
-    // window['script'+l] = document.createElement('script');
-    temp = document.createElement('script');
-    temp.type = 'application/javascript';
-    scripts.push(temp);
-
-    if(parameter_a[l]['url_cdn'] != null && address == 'localserver'){
-      temp.src = parameter_a[l]['url_primary'];
-    } else if(parameter_a[l]['url_cdn'] != null && address == 'webserver'){
-      temp.src = parameter_a[l]['url_cdn'];
+  //MARKUP BUILDER----------------------------------------------------------------
+  for(i=0; i<data.length; i++){
+    if(i === 0){
+      mode = 'href';
     } else{
-      temp.src = parameter_a[l]['url_primary'];
+      mode = 'src';
     }
-    if(parameter_a[l]['defer'] != null){
-      temp.defer = true;
-    }
-    if(parameter_a[l]['async'] != null){
-      temp.async = true;
+    for(j=0; j<data[i].length; j++){
+      if(i === 0){
+        temp = document.createElement('link');
+      } else{
+        temp = document.createElement('script');
+      }
+
+      for(k=0; k<Object.keys(data[i][j]).length; k++){
+        attrib = Object.keys(data[i][j])[k];
+
+        if(k === 0){
+          if(data[i][j][mode].length > 1 && address === 'localserver'){
+            value = data[i][j][mode][0];
+          } else if(data[i][j][mode].length > 1 && address === 'webserver'){
+            value = data[i][j][mode][1];
+          } else{
+            value = data[i][j][mode][0];
+          }
+        } else{
+          value = data[i][j][attrib];
+        }
+        temp.setAttribute(attrib, value);
+      }
+      markup.push(temp);
     }
   }
 
-  // if(address == 'localserver'){
-  //   script1.src = "../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/TweenMax.min.js";
-  //   script2.src = "../dev-modules/greensock-js/greensock-js-1.20.4/src/minified/plugins/ScrollToPlugin.min.js";
-  // } else{
-  //   script1.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js";
-  //   script2.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js";
-  // }
-
-  // script3.src = "assets/js/script.js";
-  // script4.src = "assets/layouts.js";
-  // script5.src = "assets/js/animation.js";
-  // script6.src = "assets/js/interactive.js";
-
-  for(l=0; l<scripts.length; l++){
-    document.getElementsByTagName('head')[0].insertBefore(scripts[l],null);
+  //GENERATED-MARKUP INSERTION-----------------------------------------------------------
+  for(l=0; l<markup.length; l++){
+    document.getElementsByTagName('head')[0].insertBefore(markup[l],null);
   }
-})();
+}
 
